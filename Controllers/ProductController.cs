53dcffc446e670ef9587d6895fc4580a908770ef
Bhushan.Mahajan@Telekom-Healthcare.com <Bhushan.Mahajan@Telekom-Healthcare.com>
2023-05-17 15:40:51 +0530

    Initial Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMvcWebApplication.Models;
+
+namespace MyMvcWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>() {
+        new Product() { ProductId = 100, Name = "Product1", Price = 98},
+        new Product() { ProductId = 200, Name = "Product2", Price = 50},
+        new Product() { ProductId = 300, Name = "Product3", Price = 38},
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
